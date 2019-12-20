1a97eff8265033a0b7c48d551b36b897a08b7d91
klauza <klauza.uk@gmail.com>
2019-12-20 22:24:23 +0000

    create navbar & layout setup

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import Navbar from './Navbar';
 
 import { TestComponent, JustButton, NormalButton, NormalButtonWithProps } from './reactComponentLib';
 
+const Content = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+const LeftBar = styled.div`
+  width: 100%;
+  min-height: 100vh; 
+  border-right: 1px solid #000;
+`;
 const StyledDiv = styled.div`
   padding: 10px;
   background-color: blue;
@@ -16,14 +26,22 @@ const TheBtn = styled(NormalButton)`
 class App extends Component {
   render() {
     return (
-      <div>
-        <TestComponent text="Styled Component from React library" />
-        <TheBtn text="btn"/>
-        <JustButton width="200px" />
-        <NormalButtonWithProps className="asd" style={{color: 'black', marginLeft: '25px', display: 'block'}}><span style={{color: 'purple'}}>Stolec</span></NormalButtonWithProps>
-        <NormalButton onClick={()=>console.log('asd')} />
-        <StyledDiv>Example App styled component</StyledDiv>
+      <>
+      <Navbar />
+      <div style={{display: "grid", gridTemplateColumns: "1fr 5fr"}}>
+        <LeftBar>
+          Left bar
+        </LeftBar>
+        <Content>
+          <TestComponent text="Styled Component from React library" />
+          <TheBtn text="btn"/>
+          <JustButton width="200px" />
+          <NormalButtonWithProps className="asd" style={{color: 'black', marginLeft: '25px', display: 'block'}}><span style={{color: 'purple'}}>Stolec</span></NormalButtonWithProps>
+          <NormalButton onClick={()=>console.log('asd')} />
+          <StyledDiv>Example App styled component</StyledDiv>
+        </Content>
       </div>
+      </>
     );
   }
 }
---
+++ example/src/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      In order to use that package, you need to install styled-components, best from <a href="https://www.npmjs.com/package/styled-components" >here</a> or 
+    </div>
+  )
+}
+
+export default Navbar
