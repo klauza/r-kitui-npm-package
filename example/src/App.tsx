a61b08242439422cffd204cc2d1266f8ee3929ea
klauza <klauza.uk@gmail.com>
2019-12-21 20:00:40 +0000

    file split for example page

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,47 +1,26 @@
 import React, { Component } from 'react';
+import { HashRouter, Route } from 'react-router-dom';
 import styled from 'styled-components';
 import Navbar from './Navbar';
-
-import { TestComponent, JustButton, NormalButton, NormalButtonWithProps } from './reactComponentLib';
+import Documentation from './Documentation';
+import Home from './Home';
 
 const Content = styled.div`
   width: 100%;
   height: 100%;
 `;
-const LeftBar = styled.div`
-  width: 100%;
-  min-height: 100vh; 
-  border-right: 1px solid #000;
-`;
-const StyledDiv = styled.div`
-  padding: 10px;
-  background-color: blue;
-  color: white;
-`;
-const TheBtn = styled(NormalButton)`
-  color: orange;
-  height: 25px;
-`;
 
 class App extends Component {
   render() {
     return (
-      <>
-      <Navbar />
-      <div style={{display: "grid", gridTemplateColumns: "1fr 5fr"}}>
-        <LeftBar>
-          Left bar
-        </LeftBar>
+      <HashRouter basename='/'>
         <Content>
-          <TestComponent text="Styled Component from React library" />
-          <TheBtn text="btn"/>
-          <JustButton width="200px" />
-          <NormalButtonWithProps className="asd" style={{color: 'black', marginLeft: '25px', display: 'block'}}><span style={{color: 'purple'}}>Stolec</span></NormalButtonWithProps>
-          <NormalButton onClick={()=>console.log('asd')} />
-          <StyledDiv>Example App styled component</StyledDiv>
+          <Navbar />
+
+          <Route exact path="/" component={Home} />
+          <Route path="/docs" component={Documentation} />
         </Content>
-      </div>
-      </>
+      </HashRouter>
     );
   }
 }
---
+++ example/src/Documentation.js
@@ -0,0 +1,22 @@
+import React, {Fragment} from 'react';
+import styled from 'styled-components';
+import { TestComponent, JustButton, NormalButton, NormalButtonWithProps } from './reactComponentLib';
+
+const TheBtn = styled(NormalButton)`
+  color: orange;
+  height: 25px;
+`;
+
+const Documentation = () => {
+  return (
+    <Fragment>
+      <TestComponent text="Styled Component from React library" />
+      <TheBtn text="btn"/>
+      <JustButton width="200px" />
+      <NormalButtonWithProps className="asd" style={{color: 'black', marginLeft: '25px', display: 'block'}}><span style={{color: 'purple'}}>Stolec</span></NormalButtonWithProps>
+      <NormalButton onClick={()=>console.log('asd')} />
+    </Fragment>
+  )
+}
+
+export default Documentation
