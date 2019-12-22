95315d2dfe80821aba6e9bee2556a8898146481a
klauza <klauza.uk@gmail.com>
2019-12-22 02:24:28 +0000

    Updates

---
+++ precache-manifest.fad4505d02ccfc6c8a54ae5737a066a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "124583f70341ff7c4e9e3641e0dd5e0f",
+    "url": "/r-kitui/index.html"
+  },
+  {
+    "revision": "782d61203f25bed8391b",
+    "url": "/r-kitui/static/css/main.9b641537.chunk.css"
+  },
+  {
+    "revision": "1628df6f91b6b255d48c",
+    "url": "/r-kitui/static/js/2.23830bc3.chunk.js"
+  },
+  {
+    "revision": "e5febbeaa9d79dcaccb1dc1881aa75e5",
+    "url": "/r-kitui/static/js/2.23830bc3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "782d61203f25bed8391b",
+    "url": "/r-kitui/static/js/main.82604e45.chunk.js"
+  },
+  {
+    "revision": "489a4e8d834fa1e8fcdb",
+    "url": "/r-kitui/static/js/runtime-main.331e8d5d.js"
+  }
+]);
