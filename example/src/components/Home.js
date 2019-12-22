568f3a813cd04a1ee15e8f786e8d1c675dbd83af
klauza <klauza.uk@gmail.com>
2019-12-22 02:23:21 +0000

    Home component layout update

--- example/src/components/Home.js
+++ example/src/components/Home.js
@@ -15,8 +15,17 @@ const Wrapper = styled.div`
     margin-top: 25px;
     text-align: center;
   }
+  .install-step{
+    margin: 25px 0;
+  }
   ul > li{
     list-style: inside;
+  }
+  .npm-save{
+    background: lightgrey;
+    border-radius: 5px;
+    padding: 0 5px;
+    font-family: monospace, sans-serif;
   }
 `
 const Home = () => {
@@ -27,33 +36,38 @@ const Home = () => {
         <h2>Guide</h2>
         <p style={{textAlign: 'center', textDecoration: 'underline'}}>Follow this guide to quickly set up this package into your project</p>
 
-        <div>
+        <div className="install-step">
           <p>
-            <strong>I.</strong> 
+            <strong>I. </strong> 
             First step is to install and set up local environment, skip this step if you already created your React project.
             <ul>
-              <li>Download node.js from <a href="#">here</a> and install it.</li>
-              <li>Get any code editor. Recommended and the most popular is Visual Studio Code, you can grab one from <a href="#">here</a>.</li>
-              <li>If you didn't <i style={{fontFamily: 'monospace'}}>npm create-react-app</i> yet, go for it and create a new React project.</li>
+              <li>Download node.js from <a href="https://nodejs.org/en/">here</a> and install it.</li>
+              <li>Get any code editor. Recommended and the most popular is Visual Studio Code, you can grab one from <a href="https://code.visualstudio.com/">here</a>.</li>
+              <li>If you didn't <span className="npm-save">npm create-react-app</span> yet, go for it and create a new React project.</li>
             </ul>
           </p>
         </div>
 
-        <div>
-        <p>
-          <strong>II.</strong>
-          step is to 
-        </p>
+        <div className="install-step">
+          <p>
+            <strong>II. </strong>
+            Assuming React is already implemented in your project, we can now install package peer dependencies:
+            <ul>
+              <li><span className="npm-save">npm install styled-components --save</span></li>
+            </ul>
+          </p>
         </div>
 
-        install node js
-        and code editor
-        then you may proceed for further steps of installation
+        <div className="install-step">
+          <p>
+            <strong>III. </strong>
+            Last but not least is to install react-kitui package
+            <ul>
+              <li><span className="npm-save">npm install react-kitui --save</span></li>
+            </ul>
+          </p>
+        </div>
 
-        <strong>peer dependencies</strong>
-        In order to use that package, you need to have installed styled-components in your projects
-        <strong style={{fontFamily: 'monospace'}}><i>npm i styled-components --save</i></strong>
-        
       </Wrapper>
     </Fragment>
   )
